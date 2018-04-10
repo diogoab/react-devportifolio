5c49a4e5e7a51cc1b2312673b3e9b133b7420fbe
diogoab <diogo.alves.barbosa@gmail.com>
2018-04-10 05:32:34 -0300

    create component user_profile

---
+++ src/components/user_profile/user_profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Row, Col, Card } from 'react-materialize';
+
+import avatar from '../../images/avatar.png';
+
+const UserProfile = () => (
+  <Card>
+    <Row>
+      <Col s={8} offset="s2 m2">
+        <img src={avatar} className="cicle responsive-img" />
+      </Col>
+    </Row>
+    <Row className="center-align">
+      <h5>Lorem Ipsum</h5>
+      <p className="grey darken-2 white-text">Ruby Developer</p>
+    </Row>
+  </Card>
+
+
+);
+
+export default UserProfile;
