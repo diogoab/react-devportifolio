7f8f7acb080509aedb12094987bbe774727b58f4
diogoab <diogo.alves.barbosa@gmail.com>
2018-04-10 05:18:51 -0300

    config default Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
-import Header from './components/header/header';
+import Header from './components/header/header'
+
+import Main from './main'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Main />
       </div>
     );
   }
---
+++ src/components/home/home.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Row, Col, Card } from 'react-materialize';
+
+const Home = () => (
+  <Row>
+    <Col m={3} s={12}>
+    {/* <UserProfile /> */}
+    </Col>
+    <Col m={8} s={12}>
+        <h5 className="subtitle">About Me</h5>
+        <Card>
+          <div>
+            <p><b>Lorem</b></p>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"</p>
+            <br />
+            <p><b>Ipsum</b></p>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"</p>
+          </div>
+        </Card>
+        <h5 className="subtitle">Experience</h5>
+        {/* <Experience />}
+        <Experience /> */}
+    </Col>
+  </Row>
+
+);
+
+export default Home;
